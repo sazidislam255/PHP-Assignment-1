f6903d89aa9d5a0703efa48d232466f8bb27d15c
Md. Sazid-Ul-Islam <sazidislam255@gmail.com>
2025-11-11 00:11:51 +0600

    Add files via upload

---
+++ task3.php
@@ -0,0 +1,42 @@
+<?php
+  // 1st task
+  session_start();
+  $_SESSION["name"]="Sazid Islam";
+  $_SESSION["budget"] = 12000;
+  
+  //2nd task
+  echo "Welcome,". $_SESSION['name']."! "."Your budget is ". $_SESSION['budget']."\n";
+
+  // 3rd task
+  session_destroy();
+  
+  // (4) recursive fuction
+  $sum_array = [1,2,3,4,5,6];
+  function sum($array, $size): int{
+    if($size<=0){
+        return 0;
+    }
+    else{
+        return ( sum($array, $size-1) + $array[$size-1] );
+    }
+  }
+   
+  // (6) error handling 
+  function division($a,$b){
+    try{
+        if($b==0){
+            throw new Exception ("Division by zero not possible");
+        }
+        else{
+            return $a/$b;
+        }
+    }
+    catch(Exception $error){
+        return $error -> getMessage();
+    }
+    finally{
+        echo "Error handling completed\n";
+    }
+  }
+
+?>
